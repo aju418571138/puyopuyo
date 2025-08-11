e43eee29d714d49302b354501df5f545d1e6ac5d
Seita <yasuta@example.com>
2025-08-11 17:37:45 +0900

    constructorを少し書いた

--- Puyopuyo/src/PuyoEngine/PuyoController.js
+++ Puyopuyo/src/PuyoEngine/PuyoController.js
@@ -1,5 +1,9 @@
 //ぷよぷよの操作、進行を管理するクラス
 export default class PuyoController {
+    constructor(scene, board){
+        this.scene = scene; // Phaserのシーン
+        this.board = board; // Boardクラスのインスタンス
+    }
 
 
 }
--- Puyopuyo/src/PuyoEngine/PuyoView.js
+++ Puyopuyo/src/PuyoEngine/PuyoView.js
@@ -1,4 +1,7 @@
 //盤面の描画に関するクラス
 export default class PuyoView {
-    
+    constructor(scene, board) {
+        this.scene = scene; // Phaserのシーン
+        this.board = board; // Boardクラスのインスタンス
+    }
 }
